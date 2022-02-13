b5bbfc6c28f8ce8ce294a5cc70c4625660e235b1
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-14 01:03:50 +0300

    Add a user b;ie[ront object to the user class.

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,19 @@
 export class User {
+  constructor(
+    public username: string,
+    public avatarUrl: string,
+    public name: string,
+    public bio: string,
+    public location: string,
+    public repos: string,
+    public reposLink: string
+  ) {
+      this.username = username,
+      this.avatarUrl = avatarUrl,
+      this.name = name,
+      this.bio = bio,
+      this.location = location,
+      this.repos = repos,
+      this.reposLink = reposLink
+  }
 }
