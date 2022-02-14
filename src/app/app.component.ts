5c1d8fe3af6a313f1168347e21dcebf86dc1152d
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-14 22:42:36 +0300

    Fetch repos and populate repos property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { Repo } from './blueprint/repos';
 import { Search } from './blueprint/search';
 import { FetchService } from './services/fetch.service';
 import { User } from './user';
+
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,7 @@ import { User } from './user';
 })
 export class AppComponent {
   user = new User('', '', '', '', '', '', false, 0, 0, new Date());
+  repos!: Repo[];
   errorMsg!: number;
   loading: boolean = true;
 
@@ -43,7 +46,17 @@ export class AppComponent {
 
     if (formData.searchType === 'repositories') {
       let url = `https://api.github.com/search/repositories?q=${formData.searchTerm}/`;
-      this.fetch.fetchData(url).then((res) => console.log(res));
+      this.fetch.fetchData(url).then((res) => {
+        console.log(res);
+        let response: Array<any> = res.items;
+        response.forEach((repo, index) => {
+          let repoItem: any;
+          if(index < 6){
+            repoItem = new Repo(repo.name, repo.owner.login, repo.html_url, repo.description);
+            this.repos.push(repoItem)
+          }
+        })
+      });
     }
 
     this.loading = false;
--- src/app/blueprint/repos.ts
+++ src/app/blueprint/repos.ts
@@ -1,11 +1,13 @@
-export class Repos {
+export class Repo {
   constructor(
     public repoName: string,
     public owner: string,
-    public repoUrl: string
+    public repoUrl: string,
+    public description: string
   ) {
     this.repoName = repoName;
     this.owner = owner;
     this.repoUrl = repoUrl;
+    this.description = description;
   }
 }
