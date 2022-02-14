dfd82f0789d46d7a81200a26f4adeec58cd3abde
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-14 08:06:25 +0300

    Add fetch service
    Add a fetchRandomUsers method to fetch users from the github api

---
+++ src/app/services/fetch.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FetchService {
+
+  constructor(private http:HttpClient) { }
+
+  fetchRandomUsers(url: string):Promise<any>{
+    let response:any = this.http.get(url);
+    response = lastValueFrom(response);
+    return response;
+  }
+}
