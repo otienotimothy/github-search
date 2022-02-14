94b98068542f8a891712d3da9f1ce302bff19d71
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-14 07:51:49 +0300

    Add httpclientmodule to the main app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { HeaderComponent } from './components/header/header.component';
 
+import { FetchService } from './fetch.service';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TopBarComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [FetchService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
