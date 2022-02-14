5b732b425cac44c477289c353ed244e65281866c
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-14 21:26:49 +0300

    Modify fetch service to pass the url to the fetch method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,32 +11,41 @@ import { User } from './user';
 export class AppComponent {
   user = new User('', '', '', '', '', '', false, 0, 0, new Date());
   errorMsg!: number;
-  loading: boolean = true
+  loading: boolean = true;
 
   constructor(private fetch: FetchService) {}
 
   gitSearch(formData: Search) {
     console.log(formData);
-    this.fetch
-      .fetchUser(formData.searchTerm)
-      .then((res) => {
-        console.log(res);
-        this.user.username = res.login;
-        this.user.name = res.name;
-        this.user.avatarUrl = res.avatar_url;
-        this.user.bio = res.bio;
-        this.user.location = res.location;
-        this.user.repos = res.public_repos;
-        this.user.hireable = res.hireable;
-        this.user.followers = res.followers;
-        this.user.following = res.following;
-        this.user.createdAt = new Date(res.created_at);
-        console.log(this.user);
-      })
-      .catch((err) => {
-        console.error(err);
-        this.errorMsg = err.status
-      });
-      this.loading = false;
+    if (formData.searchType === 'users') {
+      let url = `https://api.github.com/users/${formData.searchTerm}`;
+      this.fetch
+        .fetchData(url)
+        .then((res) => {
+          console.log(res);
+          this.user.username = res.login;
+          this.user.name = res.name;
+          this.user.avatarUrl = res.avatar_url;
+          this.user.bio = res.bio;
+          this.user.location = res.location;
+          this.user.repos = res.public_repos;
+          this.user.hireable = res.hireable;
+          this.user.followers = res.followers;
+          this.user.following = res.following;
+          this.user.createdAt = new Date(res.created_at);
+          console.log(this.user);
+        })
+        .catch((err) => {
+          console.error(err);
+          this.errorMsg = err.status;
+        });
+    }
+
+    if (formData.searchType === 'repositories') {
+      let url = `https://api.github.com/search/repositories?q=${formData.searchTerm}/`;
+      this.fetch.fetchData(url).then((res) => console.log(res));
+    }
+
+    this.loading = false;
   }
 }
--- src/app/services/fetch.service.ts
+++ src/app/services/fetch.service.ts
@@ -9,11 +9,9 @@ export class FetchService {
 
   constructor(private http:HttpClient) { }
 
-  fetchUser(searchTerm: string): Promise<any>{
-    let url:string = `https://api.github.com/users/${searchTerm}`
+  fetchData(url: string): Promise<any>{
     let response: any = this.http.get(url);
     response = lastValueFrom(response);
     return response;
   }
-
 }
