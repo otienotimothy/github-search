7e21978de7d80cef4c27a5796e497b621695c06a
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-14 11:13:28 +0300

    Create a user component and add it the main app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
 
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent, HeaderComponent],
+  declarations: [AppComponent, TopBarComponent, HeaderComponent, UserComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
