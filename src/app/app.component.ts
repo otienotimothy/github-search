9bc3eeb3bb800f1a94e0be7df9da6750bf28d084
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-14 08:07:11 +0300

    Add the fetch service to the main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FetchService } from './services/fetch.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'gitsearch';
+
+  randomUrl = 'https://api.github.com/users';
+
+  constructor(private fetch: FetchService) {
+    this.fetch
+      .fetchRandomUsers(this.randomUrl)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
 }
